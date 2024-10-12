a67add0dd476926924583d83b8ed84d54a3d987b
pennalminh <pennalminh@gmail.com>
2024-10-12 20:00:13 +0700

    deploy_vue_to_github

---
+++ js/about.0764b9f9.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkhandle_data"]=self["webpackChunkhandle_data"]||[]).push([[443],{4007:function(a,n,t){t.r(n),t.d(n,{default:function(){return h}});var e=t(6252);const u={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function c(a,n){return(0,e.wg)(),(0,e.iD)("div",u,s)}var d=t(3744);const i={},l=(0,d.Z)(i,[["render",c]]);var h=l}}]);
+//# sourceMappingURL=about.0764b9f9.js.map
